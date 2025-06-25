c531bbcd2ee242e3013024450dd9f008442b1663
maria_zhiganova <my.nik.mariann@gmail.com>
2025-06-25 23:50:54 +0300

    adding urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,17 @@
+import os
+
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
+
+from config import settings
+from config.settings import BASE_DIR
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls', namespace='catalog'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(BASE_DIR, 'static'))
