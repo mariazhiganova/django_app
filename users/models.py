ce49da40e9c3a379c761bc407aff8abb76c3d731
maria_zhiganova <my.nik.mariann@gmail.com>
2025-09-02 04:43:41 +0300

    add model for users

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True, help_text='Введите номер телефона')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True, verbose_name='Страна',
+                               help_text='Введите название вашей страны')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
