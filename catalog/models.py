8651c68861d249da8dfef193bfffb92bf4a20162
maria_zhiganova <my.nik.mariann@gmail.com>
2025-09-12 00:10:47 +0300

    add custom permission to unpublish in catalog model

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.db.models import DateTimeField, ForeignKey, CharField
+from django.db.models import DateTimeField, ForeignKey, CharField, BooleanField
+
+from config import settings
 
 
 class Category(models.Model):
@@ -23,6 +25,8 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена')
     created_at = DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = DateTimeField(auto_now=True, verbose_name='Дата последнего обновления')
+    is_publicate = BooleanField(verbose_name='Опубликовать', default=False)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Владелец продукта')
 
     def __str__(self):
         return self.name
@@ -31,3 +35,4 @@ class Product(models.Model):
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
         ordering = ['name', 'price', 'created_at']
+        permissions = [('can_unpublish_product', 'Can unpublish product')]
